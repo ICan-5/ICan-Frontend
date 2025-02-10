ca06bc4ced38eae4fed3522da387add9de982e91
Jiyeon Han <chocorogit@gmail.com>
2025-02-10 15:47:15 +0900

    feat: PretendardVariable 로컬 기본 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: 'I:Can',
@@ -11,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko" className={`${pretendard.variable}`}>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
