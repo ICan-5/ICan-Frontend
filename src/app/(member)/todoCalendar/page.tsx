ce7bf010dc6fa49cf658003f6900b0297472e0f8
izodam <yeon91601@gmail.com>
2025-02-12 18:23:50 +0900

    [CAN-9] feat: 날짜에 따른 할 일 목록 컴포넌트 구현

--- src/app/(member)/todoCalendar/page.tsx
+++ src/app/(member)/todoCalendar/page.tsx
@@ -1,9 +1,9 @@
-import Calendar from "@/components/todoCalendar/Calendar";
+import TodoCalendar from '@/components/todoCalendar/TodoCalendar';
 
 export default function Page() {
   return (
     <div>
-      <Calendar />
+      <TodoCalendar />
     </div>
   );
 }
