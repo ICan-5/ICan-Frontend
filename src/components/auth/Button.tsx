f321a5f43fbc7b23bf2f624c0e4b46993756fa24
Jiyeon Han <chocorogit@gmail.com>
2025-02-17 16:36:46 +0900

    [CAN-13] chore: eslint 오류 관련 수정

--- src/components/auth/Button.tsx
+++ src/components/auth/Button.tsx
@@ -3,7 +3,7 @@ type Props = {
   type?: 'submit' | 'button';
 };
 
-export default function Button({ label, type = 'button' }: Props) {
+export default function Button({ label, type }: Props) {
   return (
     <button
       className="mb-10 h-12 w-full cursor-pointer whitespace-nowrap rounded-full bg-black px-4 py-2 text-white shadow transition-colors hover:bg-grayDarkest focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
