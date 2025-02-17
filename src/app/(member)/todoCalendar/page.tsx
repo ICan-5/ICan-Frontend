8fe36cba642e06c6efd63729c9518097a7778c23
izodam <yeon91601@gmail.com>
2025-02-17 11:41:59 +0900

    [CAN-9] refactor: 로딩 컴포넌트 분리

--- src/app/(member)/todoCalendar/page.tsx
+++ src/app/(member)/todoCalendar/page.tsx
@@ -1,11 +1,5 @@
-import { Suspense } from 'react';
 import TodoCalendar from '@/components/todoCalendar/TodoCalendar';
-import Loading from './loading';
 
 export default function Page() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <TodoCalendar />
-    </Suspense>
-  );
+  return <TodoCalendar />;
 }
