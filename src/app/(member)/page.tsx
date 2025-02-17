1f8158de2cfc5293b5e62ed3be36c2073f32d923
minji kim <kmj_1027@naver.com>
2025-02-17 11:31:44 +0900

    feat: client component -> server component로 변경

--- src/app/(member)/page.tsx
+++ src/app/(member)/page.tsx
@@ -1,10 +1,11 @@
-import DashBoard from '@/components/dashboard/DashBoard';
+import TodayGraph from '@/components/dashboard/todayGraph/TodayGraph';
+import TodayList from '@/components/dashboard/todayList/TodayList';
 
 export default function Page() {
   const todayList = [
     {
       id: 1,
-      title: '자바스크립트 공부하기',
+      title: '자바스크립트 공부하기1',
       date: '2024-03-02',
       done: false,
       note: false,
@@ -18,21 +19,21 @@ export default function Page() {
     },
     {
       id: 3,
-      title: '자바스크립트 공부하기',
+      title: '자바스크립트 공부하기2',
       date: '2024-03-02',
       done: true,
       note: false,
     },
     {
       id: 4,
-      title: '자바스크립트 공부하기',
+      title: '자바스크립트 공부하기3',
       date: '2024-03-02',
-      done: false,
+      done: true,
       note: true,
     },
     {
       id: 5,
-      title: '자바스크립트 공부하기',
+      title: '자바스크립트 공부하기4',
       date: '2024-03-02',
       done: false,
       note: false,
@@ -40,19 +41,32 @@ export default function Page() {
 
     {
       id: 6,
-      title: '자바스크립트 공부하기',
+      title: '자바스크립트 공부하기5',
       date: '2024-03-02',
       done: false,
       note: false,
     },
     {
       id: 7,
-      title: '자바스크립트 공부하기',
+      title: '자바스크립트 공부하기6',
       date: '2024-03-02',
       done: false,
       note: true,
     },
   ];
 
-  return <DashBoard today={todayList} />;
+  return (
+    <div className="relative left-1/2 flex max-h-[1000px] w-full max-w-6xl -translate-x-1/2 flex-col gap-4 p-4 md:h-screen md:gap-8 md:p-10">
+      <section className="flex flex-col gap-4 overflow-hidden md:h-1/3 md:flex-row md:gap-8">
+        <TodayList todayList={todayList} />
+        <TodayGraph />
+      </section>
+      <section className="flex h-1/3 gap-8">
+        <TodayGraph />
+      </section>
+      <section className="flex h-1/3 gap-8">
+        <TodayGraph />
+      </section>
+    </div>
+  );
 }
