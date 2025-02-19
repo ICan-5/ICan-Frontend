dacfb8831c9c5a27784727c1da06d60ae4e61994
Jiyeon Han <chocorogit@gmail.com>
2025-02-19 15:05:53 +0900

    [CAN-29] refactor: 회원가입 유효성 검사 메시지를 별도 파일로 분리

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,5 +1,22 @@
 import { z } from 'zod';
+import { EMAIL_VALIDATION, PASSWORD_VALIDATION } from './regexp';
 
+const ERROR_MESSAGE = {
+  name: {
+    empty: '이름을 입력해주세요',
+    max: '이름은 최대 10자 입력 가능합니다',
+  },
+  email: {
+    invalid: '올바른 이메일 형식을 입력해주세요',
+  },
+  password: {
+    invalid:
+      '비밀번호는 공백을 제외, 영문 대소문자, 숫자, 특수문자를 포함한 최소 8자 이상이어야 합니다',
+    min: '비밀번호는 최소 8자 이상 입력 가능합니다',
+    max: '비밀번호는 최대 12자 입력 가능합니다',
+    match: '비밀번호가 일치하지 않습니다',
+  },
+};
 // 객체 스키마 생성
 // 회원가입 스키마
 export const SignUpSchema = z
@@ -7,46 +24,33 @@ export const SignUpSchema = z
     name: z
       .string()
       .trim()
-      .min(1, { message: '이름을 입력해주세요' })
-      .max(10, { message: '이름은 최대 10자 입력 가능합니다' }),
-    email: z
-      .string()
-      // eslint-disable-next-line
-      .regex(/^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
-        message: '올바른 이메일 형식을 입력해주세요',
-      }),
+      .min(1, { message: ERROR_MESSAGE.name.empty })
+      .max(10, { message: ERROR_MESSAGE.name.max }),
+    email: z.string().regex(EMAIL_VALIDATION, {
+      message: ERROR_MESSAGE.email.invalid,
+    }),
     password: z
       .string()
       .trim()
-      .regex(
-        // eslint-disable-next-line
-        /^(?!.*\s)(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
-        {
-          message:
-            '비밀번호는 공백을 제외, 영문 대소문자, 숫자, 특수문자를 포함한 최소 8자 이상이어야 합니다',
-        },
-      )
-      .min(8, { message: '비밀번호는 최소 8자 이상 입력 가능합니다' })
-      .max(12, { message: '비밀번호는 최대 12자 입력 가능합니다' }),
+      .regex(PASSWORD_VALIDATION, {
+        message: ERROR_MESSAGE.password.invalid,
+      })
+      .min(8, { message: ERROR_MESSAGE.password.min })
+      .max(12, { message: ERROR_MESSAGE.password.max }),
     confirmPassword: z
       .string()
       .trim()
-      .regex(
-        // eslint-disable-next-line
-        /^(?!.*\s)(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
-        {
-          message:
-            '비밀번호는 공백을 제외, 영문 대소문자, 숫자, 특수문자를 포함한 최소 8자 이상이어야 합니다',
-        },
-      )
-      .min(8, { message: '비밀번호는 최소 8자 이상 입력 가능합니다' })
-      .max(12, { message: '비밀번호는 최대 12자 입력 가능합니다' }),
+      .regex(PASSWORD_VALIDATION, {
+        message: ERROR_MESSAGE.password.invalid,
+      })
+      .min(8, { message: ERROR_MESSAGE.password.min })
+      .max(12, { message: ERROR_MESSAGE.password.max }),
   })
   .superRefine(({ confirmPassword, password }, ctx) => {
     if (confirmPassword !== password) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
-        message: '비밀번호와 비밀번호 확인이 일치하지 않습니다',
+        message: ERROR_MESSAGE.password.match,
         path: ['confirmPassword'],
       });
     }
@@ -54,23 +58,17 @@ export const SignUpSchema = z
 
 // 로그인 스키마
 export const LoginSchema = z.object({
-  // eslint-disable-next-line
-  id: z.string().regex(/^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
-    message: '올바른 이메일 형식을 입력해주세요',
+  id: z.string().regex(EMAIL_VALIDATION, {
+    message: ERROR_MESSAGE.email.invalid,
   }),
   password: z
     .string()
     .trim()
-    .regex(
-      // eslint-disable-next-line
-      /^(?!.*\s)(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
-      {
-        message:
-          '비밀번호는 공백을 제외, 영문 대소문자, 숫자, 특수문자를 포함한 최소 8자 이상이어야 합니다',
-      },
-    )
-    .min(8, { message: '비밀번호는 최소 8자 이상 입력 가능합니다' })
-    .max(12, { message: '비밀번호는 최대 12자 입력 가능합니다' }),
+    .regex(PASSWORD_VALIDATION, {
+      message: ERROR_MESSAGE.password.invalid,
+    })
+    .min(8, { message: ERROR_MESSAGE.password.min })
+    .max(12, { message: ERROR_MESSAGE.password.max }),
 });
 
 // 타입스크립트를 사용하고 있으므로 스키마의 z.infer를 사용하여 스키마 유형도 내보내야 함
