2f9f519995a49617443ea287075a8cfc87e9a0c1
minji kim <kmj_1027@naver.com>
2025-02-19 13:38:40 +0900

    [CAN-19] feat: 퍼센티지에 따라 문구 변경

--- src/components/dashboard/todayProgress/TodayProgress.tsx
+++ src/components/dashboard/todayProgress/TodayProgress.tsx
@@ -6,8 +6,18 @@ type Props = {
 };
 
 export default function TodayProgress({ progress }: Props) {
-  const progressMessage =
-    progress > 0 ? '잘 하고 있어요!' : '차근차근 시작해볼까요?';
+  /**
+   * @returns 진행도에 따른 문구
+   * prgress에 맞는 문구를 리턴해주는 함수
+   */
+  const getProgressMessage = () => {
+    if (progress <= 10) return '차근차근 시작해볼까요?';
+    if (progress <= 30) return '좋아요! 조금씩 진행해 봐요';
+    if (progress <= 70) return '잘하고 있어요! 계속 가볼까요?';
+    if (progress < 100) return '거의 다 왔어요! 마지막까지 힘내요!';
+    return '대단해요! 오늘도 목표 달성!';
+  };
+
   return (
     <div className="relative flex h-full min-h-48 w-full flex-1 flex-col rounded-2xl border-2 border-gs200 px-6 py-4 2xl:rounded-3xl">
       <p className="text-16SB 2xl:text-18SB">금일 진행도</p>
@@ -19,7 +29,7 @@ export default function TodayProgress({ progress }: Props) {
         )}
       >
         {Math.floor(progress * 100)}%
-        <span className="text-12R text-gs600">{progressMessage}</span>
+        <span className="text-12R text-gs600">{getProgressMessage()}</span>
       </p>
     </div>
   );
