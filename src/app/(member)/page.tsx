a8936ae8ade9d670a1af431706b89b50de316a0b
minji kim <kmj_1027@naver.com>
2025-02-19 11:04:10 +0900

    [CAN-19] chore: 배경 색 변경, page에서 TodayGraph -> TodayProgress

--- src/app/(member)/page.tsx
+++ src/app/(member)/page.tsx
@@ -1,4 +1,4 @@
-import TodayGraph from '@/components/dashboard/todayGraph/TodayGraph';
+import TodayProgress from '@/components/dashboard/todayGraph/TodayProgress';
 import TodayList from '@/components/dashboard/todayList/TodayList';
 
 export default function Page() {
@@ -7,7 +7,7 @@ export default function Page() {
       id: 1,
       title: '자바스크립트 공부하기1',
       date: '2024-03-02',
-      done: false,
+      done: true,
       note: false,
     },
     {
@@ -55,18 +55,18 @@ export default function Page() {
     },
   ];
 
+  const completedCount = todayList.filter((e) => e.done).length;
+  const totalCount = todayList.length;
+  const progress = Math.floor((completedCount / totalCount) * 100) / 100;
+
   return (
-    <div className="relative left-1/2 flex max-h-[1000px] w-full max-w-6xl -translate-x-1/2 flex-col gap-4 p-4 md:h-screen md:gap-8 md:p-10">
-      <section className="flex flex-col gap-4 overflow-hidden md:h-1/3 md:flex-row md:gap-8">
+    <div className="relative left-1/2 flex max-h-[1000px] w-full max-w-6xl -translate-x-1/2 flex-col gap-4 overflow-auto p-4 md:h-screen md:gap-8 md:p-10">
+      <section className="flex min-h-48 flex-col gap-4 overflow-hidden md:h-1/3 md:flex-row md:gap-8">
         <TodayList todayList={todayList} />
-        <TodayGraph />
+        <TodayProgress progress={progress} />
       </section>
-      <section className="flex h-1/3 gap-8">
-        <TodayGraph />
-      </section>
-      <section className="flex h-1/3 gap-8">
-        <TodayGraph />
-      </section>
+      <section className="flex h-1/3 gap-8" />
+      <section className="flex h-1/3 gap-8" />
     </div>
   );
 }
