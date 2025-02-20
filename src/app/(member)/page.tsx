3c0ae26fddc0ce7d3d5fd2726b7875fb56f8ee2f
minji kim <kmj_1027@naver.com>
2025-02-20 16:05:22 +0900

    [CAN-22] chore: 스타일, todo 구조 변경

--- src/app/(member)/page.tsx
+++ src/app/(member)/page.tsx
@@ -8,35 +8,35 @@ export default function Page() {
       title: '자바스크립트 공부하기1',
       date: '2024-03-02',
       done: true,
-      note: false,
+      noteId: null,
     },
     {
       id: 2,
       title: '자바스크립트 공부하ewfwefwefwfwefwefwfefwefwefwef기',
       date: '2024-03-02',
       done: false,
-      note: true,
+      noteId: 3,
     },
     {
       id: 3,
       title: '자바스크립트 공부하기2',
       date: '2024-03-02',
       done: true,
-      note: false,
+      noteId: 1,
     },
     {
       id: 4,
       title: '자바스크립트 공부하기3',
       date: '2024-03-02',
       done: true,
-      note: true,
+      noteId: null,
     },
     {
       id: 5,
       title: '자바스크립트 공부하기4',
       date: '2024-03-02',
       done: false,
-      note: false,
+      noteId: 3,
     },
 
     {
@@ -44,14 +44,14 @@ export default function Page() {
       title: '자바스크립트 공부하기5',
       date: '2024-03-02',
       done: false,
-      note: false,
+      noteId: null,
     },
     {
       id: 7,
       title: '자바스크립트 공부하기6',
       date: '2024-03-02',
       done: false,
-      note: true,
+      noteId: 2,
     },
   ];
 
@@ -60,7 +60,7 @@ export default function Page() {
   const progress = Math.floor((completedCount / totalCount) * 100) / 100;
 
   return (
-    <div className="relative left-1/2 flex max-h-[1000px] w-full max-w-6xl -translate-x-1/2 flex-col gap-4 overflow-auto p-4 md:h-screen md:gap-8 md:p-10">
+    <div className="relative left-1/2 flex max-h-[1000px] w-full max-w-screen-2xl -translate-x-1/2 flex-col gap-4 overflow-auto p-4 md:h-screen md:gap-8 md:p-10">
       <section className="flex min-h-48 flex-col gap-4 overflow-hidden md:h-1/3 md:flex-row md:gap-8">
         <TodayList todayList={todayList} />
         <TodayProgress progress={progress} />
