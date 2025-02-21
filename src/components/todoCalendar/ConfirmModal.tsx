97a58c38a0485f24e7b411abfef3694844e8b07b
izodam <yeon91601@gmail.com>
2025-02-21 10:29:48 +0900

    [CAN-15] style: confirm 창 이미지 아이콘으로 변경

--- src/components/todoCalendar/ConfirmModal.tsx
+++ src/components/todoCalendar/ConfirmModal.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type Props = {
   onClose: () => void;
@@ -7,15 +8,16 @@ type Props = {
 
 function ConfirmModal({ onClose, onConfirm }: Props) {
   return (
-    <div className="flex w-[450px] flex-col gap-6 rounded-lg bg-white p-6">
+    <div className="flex w-[450px] flex-col gap-6 rounded-lg bg-gs00 p-6">
       <div className="flex w-full flex-col items-center justify-center gap-1">
-        <Image
-          className="px-4 py-2"
-          src="/images/exclude.png"
-          width="84"
-          height="68"
-          alt="excludeImage"
-        />
+        <div className="px-4 py-2">
+          <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gray-400">
+            <FontAwesomeIcon
+              icon={faExclamation}
+              className="text-3xl text-gs00"
+            />
+          </div>
+        </div>
         <span className="text-18M">정말 나가시겠어요?</span>
         <span className="text-14M text-gs400">
           작성한 내용이 모두 사라집니다.
