1133ac1b879b78a1c734c9c1382226330ebb54dd
Jiyeon Han <chocorogit@gmail.com>
2025-02-24 02:37:59 +0900

    [CAN-26] feat: 유저 프로필에 로그인한 사용자 정보 연결

--- src/components/common/navbar/NavUserProfile.tsx
+++ src/components/common/navbar/NavUserProfile.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { signOut, useSession } from 'next-auth/react';
 import cn from '@/utils/cn';
 
 type Props = {
@@ -8,11 +9,17 @@ type Props = {
 export default function NavUserProfile({ isFolded }: Props) {
   // TODO:: API연결 되면 밑 데이터 삭제
   // 프로필 데이터 받아오기
-  const data = {
-    image: '',
-    name: 'Jack Martine',
-    email: 'sdfweffwefw@gmail.com',
-  };
+  const { data, status } = useSession(); // 세션 가져오기
+
+  if (status === 'loading') {
+    return <p>사용자 정보 로딩중...</p>;
+  }
+
+  if (!data) {
+    return <p>로그인 세션을 찾을 수 없습니다. 다시 로그인 해주세요</p>;
+  }
+
+  const { user } = data;
 
   return (
     <div className="my-6 flex h-16 w-full flex-none items-center gap-3 overflow-hidden 2xl:my-8">
@@ -21,7 +28,7 @@ export default function NavUserProfile({ isFolded }: Props) {
           'h-12 w-12 transition-all duration-300 2xl:h-16 2xl:w-16',
           { 'ml-1 scale-0 2xl:h-10 2xl:w-10': isFolded },
         )}
-        src={data.image || '/images/profile.png'}
+        src={user?.image || '/images/profile.png'}
         width="64"
         height="64"
         alt="profileImage"
@@ -29,11 +36,14 @@ export default function NavUserProfile({ isFolded }: Props) {
       />
       <div className="flex w-full flex-col gap-1 overflow-hidden">
         <span className="text-overflow w-full text-14SB font-semibold text-gsBk">
-          {data.name}
+          {user?.name}
         </span>
         <span className="text-overflow w-full text-12M text-gs400">
-          {data.email}
+          {user?.email}
         </span>
+        <button type="button" onClick={() => signOut()}>
+          로그아웃
+        </button>
       </div>
     </div>
   );
