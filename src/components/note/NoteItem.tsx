50ff3b1ebaa2cc205f4aded421ddbd8550e2991c
minji kim <kmj_1027@naver.com>
2025-02-24 11:13:26 +0900

    [CAN-22] feat: useClickOustside state까지 포함하도록 수정

--- src/components/note/NoteItem.tsx
+++ src/components/note/NoteItem.tsx
@@ -2,7 +2,7 @@
 
 import { faLayerGroup, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import React from 'react';
 import { useClickOutside } from '@/hooks/useClickOutside';
 
 type Note = {
@@ -16,10 +16,8 @@ type NoteItemProps = {
 };
 
 export default function NoteItem({ note }: NoteItemProps) {
-  const [openMenu, setOpenMenu] = useState(false);
-  const menuRef = useClickOutside<HTMLDivElement>(() => {
-    setOpenMenu(false);
-  });
+  const [menuRef, isMenuOpen, setIsMenuOpen] =
+    useClickOutside<HTMLDivElement>();
 
   return (
     <div className="mb-4 w-4/5 rounded-2xl border bg-gs00 p-4 shadow-md">
@@ -34,9 +32,9 @@ export default function NoteItem({ note }: NoteItemProps) {
           <FontAwesomeIcon
             icon={faEllipsisV}
             className="cursor-pointer text-gs500"
-            onClick={() => setOpenMenu((prev) => !prev)}
+            onClick={() => setIsMenuOpen((prev) => !prev)}
           />
-          {openMenu && (
+          {isMenuOpen && (
             <div className="absolute right-0 z-10 mt-2 w-24 rounded bg-gs00 shadow-md">
               <button
                 type="button"
