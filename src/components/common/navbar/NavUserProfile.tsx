c0e49a5daa63d1ef8ee896c3034c7aae71900b56
minji kim <kmj_1027@naver.com>
2025-02-24 17:02:28 +0900

    [CAN-42] feat: navbar user정보 연결, 로딩 중 skeleton적용

--- src/components/common/navbar/NavUserProfile.tsx
+++ src/components/common/navbar/NavUserProfile.tsx
@@ -7,39 +7,39 @@ type Props = {
 };
 
 export default function NavUserProfile({ isFolded }: Props) {
-  // TODO:: API연결 되면 밑 데이터 삭제
-  // 프로필 데이터 받아오기
-  const { data, status } = useSession(); // 세션 가져오기
-
-  if (status === 'loading') {
-    return <p>사용자 정보 로딩중...</p>;
-  }
-
-  if (!data) {
-    return <p>로그인 세션을 찾을 수 없습니다. 다시 로그인 해주세요</p>;
-  }
-
-  const { user } = data;
+  const { data, status } = useSession();
 
   return (
     <div className="my-6 flex h-16 w-full flex-none items-center gap-3 overflow-hidden 2xl:my-8">
       <Image
         className={cn(
-          'h-12 w-12 transition-all duration-300 2xl:h-16 2xl:w-16',
-          { 'ml-1 opacity-0 2xl:h-10 2xl:w-10': isFolded },
+          'size-12 rounded-full transition-all duration-300 2xl:size-16',
+          { 'ml-1 opacity-0 2xl:size-10': isFolded },
         )}
-        src={user?.image || '/images/profile.png'}
+        src={data?.user?.image || '/images/profile.png'}
         width="64"
         height="64"
         alt="profileImage"
         priority
       />
       <div className="flex w-full flex-col gap-1 overflow-hidden">
-        <span className="text-overflow w-full text-14SB font-semibold text-gsBk">
-          {user?.name}
+        <span
+          className={cn(
+            'w-full overflow-hidden whitespace-nowrap break-words text-14SB font-semibold text-gsBk 2xl:text-16SB',
+            { 'h-5 animate-pulse rounded-sm bg-gs100': status === 'loading' },
+          )}
+        >
+          {data?.user?.name}
         </span>
-        <span className="text-overflow w-full text-12M text-gs400">
-          {user?.email}
+        <span
+          className={cn(
+            'w-full overflow-hidden whitespace-nowrap break-words text-12M text-gs400 2xl:text-14M',
+            {
+              'h-4 animate-pulse rounded-sm bg-gs100': status === 'loading',
+            },
+          )}
+        >
+          {data?.user?.email}
         </span>
       </div>
     </div>
