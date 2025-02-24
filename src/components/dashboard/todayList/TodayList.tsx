b55ce9877a2ef5af88a8935896dd3a285b0a58ba
Jiyeon Han <chocorogit@gmail.com>
2025-02-24 15:06:10 +0900

    [CAN-26] feat: 로그인 사용자 세션을 통해 대시보드 오늘 할일에 이름 표시

--- src/components/dashboard/todayList/TodayList.tsx
+++ src/components/dashboard/todayList/TodayList.tsx
@@ -1,10 +1,8 @@
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons/faAngleRight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
-import Button from '@/components/common/button/Button';
-import Icon from '@/components/common/icon/Icon';
-import SimpleTodo from '@/components/common/todo/SimpleTodo';
+import TodayListItem from './TodayListItem';
+import { auth } from '@/auth';
 
 // TOOD:: 나중에 할일 Type정해지면 todolist: Todo[]
 type Props = {
@@ -17,18 +15,27 @@ type Props = {
   }[];
 };
 
-export default function TodayList({ todayList }: Props) {
+export default async function TodayList({ todayList }: Props) {
   const formatter = new Intl.DateTimeFormat('ko-KR', { dateStyle: 'long' });
   const formattedDate = formatter.format(new Date());
 
   const todoList = todayList.filter((e) => !e.done);
+  const doneList = todayList.filter((e) => e.done);
+
+  const session = await auth(); // 세션 가져오기
+
+  if (!session) {
+    return <p>로그인 유저를 찾을 수 없습니다. 다시 로그인 해주세요</p>;
+  }
+
+  const { user } = session;
 
   return (
     <div className="relative flex min-h-48 w-full flex-1 flex-col overflow-hidden rounded-2xl bg-white p-4 md:h-full md:px-6 md:py-4">
       <section className="mb-2 flex w-full flex-none items-start">
         <p className="mr-auto flex flex-col">
           <span className="text-lg">
-            안녕, <strong className="text-slate500">민지</strong>! 🖐️
+            안녕, <strong className="text-slate500">{user?.name}</strong>! 🖐️
           </span>
           <span className="text-sm text-gray-400">{formattedDate}</span>
         </p>
