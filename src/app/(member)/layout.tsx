f33fef1eca3c25f99f3a7f6e8a0afb4385f68a7b
minji kim <kmj_1027@naver.com>
2025-02-26 16:46:01 +0900

    [CAN-42] style: md까지 navbar fixed

--- src/app/(member)/layout.tsx
+++ src/app/(member)/layout.tsx
@@ -8,8 +8,10 @@ export default function Layout({
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-gs100 will-change-scroll">
       <Navbar />
-      <div className="ml-16 flex-1 overflow-y-auto overscroll-contain sm:ml-0">
-        {children}
+      <div className="ml-16 flex-1 overflow-y-auto overscroll-contain md:ml-0">
+        <div className="relative left-1/2 flex max-h-[1000px] w-full max-w-screen-xl -translate-x-1/2 flex-col gap-4 overflow-auto p-4 md:h-screen md:gap-8 md:p-10">
+          {children}
+        </div>
       </div>
     </div>
   );
