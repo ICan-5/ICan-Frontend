f23f48148f5dd80f652cd2192b130f3d41681d07
Jiyeon Han <chocorogit@gmail.com>
2025-02-27 18:11:37 +0900

    update 변경;

--- src/auth.ts
+++ src/auth.ts
@@ -2,13 +2,7 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 
-export const {
-  auth,
-  handlers,
-  signIn,
-  signOut,
-  unstable_update: update,
-} = NextAuth({
+export const { auth, handlers, signIn, signOut, update } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
