588cf929e415990f7a2a5dbd0679b458e27eace0
izodam <yeon91601@gmail.com>
2025-02-27 19:13:51 +0900

    [CAN-23] style: 옵션 케밥 open시 hover와 동일하게 표시

--- src/components/common/todo/CheckTodo.tsx
+++ src/components/common/todo/CheckTodo.tsx
@@ -62,6 +62,7 @@ export default function CheckTodo({
         className={cn(
           'group flex w-full cursor-pointer items-center justify-center gap-2 border-b border-dashed border-gs200 p-2 text-gsBk 2xl:gap-3 2xl:px-3 2xl:py-4',
           { 'hover:bg-slate50 hover:text-slate700': !done },
+          { 'bg-slate50 text-slate700': isMenuOpen },
         )}
         onClick={() => {
           if (onCheck) onCheck();
@@ -104,9 +105,10 @@ export default function CheckTodo({
         </p>
         <IconButton
           className={cn(
-            'flex-none rounded-2xl bg-gs50 text-slate500 group-hover:bg-gs00',
+            'flex-none rounded-2xl bg-gs00 text-slate500 group-hover:bg-gs00',
             {
-              'invisible pl-1 group-hover:visible': noteId === null,
+              'opacity-100': isMenuOpen,
+              'opacity-0 group-hover:opacity-100': !isMenuOpen,
             },
           )}
           icon={noteIcon}
@@ -117,7 +119,13 @@ export default function CheckTodo({
         />
         <div className="relative">
           <IconButton
-            className="flex-none rounded-2xl bg-gs50 text-gs400 group-hover:bg-gs00 md:hidden md:group-hover:flex"
+            className={cn(
+              'flex-none rounded-2xl bg-gs00 text-gs400 transition-opacity',
+              {
+                'opacity-100': isMenuOpen,
+                'opacity-0 group-hover:opacity-100': !isMenuOpen,
+              },
+            )}
             icon={faEllipsisVertical}
             onClick={(e) => {
               e.stopPropagation();
