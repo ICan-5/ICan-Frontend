82f45922accbd5e42ac8251522dd019727ad576f
minji kim <kmj_1027@naver.com>
2025-02-28 09:10:24 +0900

    [CAN-21] feat: 목표별 할일 컴포넌트 개발

--- src/app/(member)/page.tsx
+++ src/app/(member)/page.tsx
@@ -4,6 +4,7 @@ import TodayProgress from '@/components/dashboard/todayProgress/TodayProgress';
 import TodayList from '@/components/dashboard/todayList/TodayList';
 import TodayProgressSkeleton from '@/components/dashboard/todayProgress/TodayProgressSkeleton';
 import TodayProgressError from '@/components/dashboard/todayProgress/TodayProgressError';
+import GoalList from '@/components/dashboard/goalList/GoalList';
 
 export default function Page() {
   return (
@@ -16,8 +17,11 @@ export default function Page() {
           </Suspense>
         </ErrorBoundary>
       </section>
-      <section className="flex flex-col" />
-      <section className="flex" />
+      <section className="flex flex-col">
+        <p className="text-16SB 2xl:text-18SB">목표별 할일</p>
+        <GoalList />
+      </section>
+      <section className="flex gap-8" />
     </div>
   );
 }
