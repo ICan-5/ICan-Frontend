2c9fca91a0afafabc7f96efe61c1ee91d8004f78
minji kim <kmj_1027@naver.com>
2025-02-28 13:21:47 +0900

    [CAN-46] feat: 에러바운더리, getProgress 에러 임시 처리

--- src/app/(member)/page.tsx
+++ src/app/(member)/page.tsx
@@ -1,16 +1,20 @@
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 import TodayProgress from '@/components/dashboard/todayProgress/TodayProgress';
 import TodayList from '@/components/dashboard/todayList/TodayList';
 import TodayProgressSkeleton from '@/components/dashboard/todayProgress/TodayProgressSkeleton';
+import TodayProgressError from '@/components/dashboard/todayProgress/TodayProgressError';
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-4">
       <section className="flex flex-col gap-4 overflow-hidden md:flex-row md:gap-8">
         <TodayList />
-        <Suspense fallback={<TodayProgressSkeleton />}>
-          <TodayProgress />
-        </Suspense>
+        <ErrorBoundary errorComponent={TodayProgressError}>
+          <Suspense fallback={<TodayProgressSkeleton />}>
+            <TodayProgress />
+          </Suspense>
+        </ErrorBoundary>
       </section>
       <section className="flex flex-col" />
       <section className="flex" />
