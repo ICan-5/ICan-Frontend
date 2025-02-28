57721a307f4f27c1019c8c03bb2e3a5b8045bee6
minji kim <kmj_1027@naver.com>
2025-02-28 09:10:03 +0900

    [CAN-21] feat: 목표별 할일 목표 탭 개발

---
+++ src/components/dashboard/goalList/GoalTab.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { MouseEvent } from 'react';
+import cn from '@/utils/cn';
+
+interface Props {
+  title: string;
+  isSelected: boolean;
+  onSelect: () => void;
+}
+
+export default function GoalTab({ title, isSelected, onSelect }: Props) {
+  const clickTab = (event: MouseEvent<HTMLButtonElement>) => {
+    onSelect();
+    event.currentTarget?.scrollIntoView({
+      behavior: 'smooth', // 부드러운 스크롤
+      inline: 'center', // 가로 중앙 정렬
+      block: 'nearest', // 세로 위치 유지
+    });
+  };
+
+  return (
+    <button
+      type="button"
+      className={cn(
+        'text-overflow relative h-9 flex-none rounded-t-lg bg-gs50 px-3 py-2 text-left text-14M transition-all duration-150',
+        { 'w-40 bg-gs00 md:w-56': isSelected },
+        { 'w-20 border-x border-t border-gs200 md:w-32': !isSelected },
+      )}
+      onClick={clickTab}
+    >
+      <span
+        className={cn(
+          'invisible absolute left-0 top-1/2 h-6 w-[2px] -translate-y-1/2 bg-slate500',
+          { visible: isSelected },
+        )}
+      />
+      {title}
+    </button>
+  );
+}
