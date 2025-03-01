0937db85f409157feb1e247d483c6a740b3f3763
izodam <yeon91601@gmail.com>
2025-03-01 19:36:53 +0900

    [CAN-56] fix: 코드래빗 제안 수정

--- src/app/(member)/@modal/(.)note/[noteId]/page.tsx
+++ src/app/(member)/@modal/(.)note/[noteId]/page.tsx
@@ -1,23 +1,10 @@
-import NoteModal from '@/components/note/NoteModal';
-import { getNoteDetail } from '@/services/note';
+import { renderNoteDetail } from '@/utils/renderNotePage';
 
-export default async function PhotoModal({
+export default async function NoteDetailModal({
   params,
 }: {
   params: { noteId: string };
 }) {
   const noteId = Number(params.noteId);
-
-  const note = await getNoteDetail(noteId);
-  return (
-    note && (
-      <NoteModal
-        goalTitle={note.goalTitle}
-        todoTitle={note.todoTitle}
-        title={note.title}
-        content={note.content}
-        updatedAt={note.updatedAt}
-      />
-    )
-  );
+  return renderNoteDetail(noteId);
 }
