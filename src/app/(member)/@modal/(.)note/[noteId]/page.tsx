f186fef7c5acc7e98cbbc8ecbe6096ac9ffa6a9a
izodam <yeon91601@gmail.com>
2025-03-01 19:27:11 +0900

    [CAN-56] fix: note 비어있는 경우 오류 수정

--- src/app/(member)/@modal/(.)note/[noteId]/page.tsx
+++ src/app/(member)/@modal/(.)note/[noteId]/page.tsx
@@ -7,15 +7,17 @@ export default async function PhotoModal({
   params: { noteId: string };
 }) {
   const noteId = Number(params.noteId);
-  const note = await getNoteDetail(noteId);
 
+  const note = await getNoteDetail(noteId);
   return (
-    <NoteModal
-      goalTitle={note.goalTitle}
-      todoTitle={note.todoTitle}
-      title={note.title}
-      content={note.content}
-      updatedAt={note.updatedAt}
-    />
+    note && (
+      <NoteModal
+        goalTitle={note.goalTitle}
+        todoTitle={note.todoTitle}
+        title={note.title}
+        content={note.content}
+        updatedAt={note.updatedAt}
+      />
+    )
   );
 }
