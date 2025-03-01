63141b2eda55ca9f12ccb64cda08214678a1770e
Jiyeon Han <chocorogit@gmail.com>
2025-03-02 03:37:08 +0900

    [CAN-27] fix: 클래스 이름 처리 방식 cn으로 수정

--- src/components/auth/ErrorMessage.tsx
+++ src/components/auth/ErrorMessage.tsx
@@ -1,9 +1,11 @@
+import cn from '@/utils/cn';
+
 interface Props extends React.HTMLAttributes<HTMLParagraphElement> {
   message: string;
 }
 export default function ErrorMessage({ message, className }: Props) {
   return (
-    <p className={`ml-2 mt-1 break-keep text-14M text-warn500 ${className}`}>
+    <p className={cn('ml-2 mt-1 break-keep text-14M text-warn500', className)}>
       {message}
     </p>
   );
