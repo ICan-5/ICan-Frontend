68f0872bb7e808e16603ab394d81cde3cebc5f16
minji kim <kmj_1027@naver.com>
2025-03-03 04:36:52 +0900

    [CAN-20] feat: 잔디 UI 개발

---
+++ src/components/dashboard/todoGrass/TodoGrassCell.tsx
@@ -0,0 +1,28 @@
+import cn from '@/utils/cn';
+
+interface Props {
+  date: string;
+  progress: number;
+}
+
+export default function TodoGrassCell({ date, progress }: Props) {
+  const currentYear = `${new Date().getFullYear()}`;
+
+  const getCellColor = () => {
+    if (progress === 0) return 'bg-slate50';
+    if (progress < 20) return 'bg-slate100'; // 연한 색
+    if (progress < 40) return 'bg-slate200'; // 조금 더 진한 색
+    if (progress < 60) return 'bg-slate300'; // 중간색
+    if (progress < 80) return 'bg-slate400'; // 진한 색
+    if (progress < 100) return 'bg-slate500';
+    return 'bg-slate600'; // 매우 진한 색
+  };
+
+  return (
+    <li
+      className={cn(`size-4 flex-none rounded-sm ${getCellColor()}`, {
+        invisible: currentYear !== date.slice(0, 4),
+      })}
+    />
+  );
+}
