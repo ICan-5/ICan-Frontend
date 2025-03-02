6938802ae3cf432e3b15071cb195ef41a2aab77a
Jiyeon Han <chocorogit@gmail.com>
2025-03-03 03:07:40 +0900

    [CAN-27] fix: 노트 작성 폼, NoteEditor, NoteTitle 컴포넌트 분리 및 실시간 유효성 검사 수정, 글자 수 세기 수정

---
+++ src/components/note/NoteContentEditor.tsx
@@ -0,0 +1,100 @@
+import React, { useMemo, useRef, useState } from 'react';
+import 'react-quill-new/dist/quill.snow.css';
+import dynamic from 'next/dynamic';
+import type ReactQuillType from 'react-quill-new';
+import { Controller } from 'react-hook-form';
+import ErrorMessage from '../auth/ErrorMessage';
+import { Props } from '@/types/note';
+
+interface ForwardedQuillProps
+  extends React.ComponentProps<typeof ReactQuillType> {
+  forwardedRef: React.Ref<ReactQuillType>;
+}
+
+const QuillNoSSRWrapper = dynamic(
+  async () => {
+    const { default: QuillComponent } = await import('react-quill-new');
+    function Quill({ forwardedRef, ...props }: ForwardedQuillProps) {
+      /* eslint-disable react/jsx-props-no-spreading */
+      return <QuillComponent ref={forwardedRef} {...props} />;
+    }
+    return Quill;
+  },
+  { loading: () => <div>...loading</div>, ssr: false },
+);
+
+// 툴바 옵션들
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],
+  [{ align: '' }, { align: 'center' }, { align: 'right' }],
+
+  [{ list: 'bullet' }, { list: 'ordered' }, { color: [] }, { background: [] }],
+  ['link'],
+];
+export default function NoteContentEditor({ control, errors }: Props) {
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: toolbarOptions,
+      },
+    };
+  }, []);
+
+  const [textLength, setTextLength] = useState(0);
+  const [trimmedTextLength, setTrimmedTextLength] = useState(0);
+  const quillInstance = useRef<ReactQuillType | null>(null);
+
+  const isEmptyContent = useMemo(
+    () => (html: string) => {
+      return html.replace(/<[^>]*>/g, '').replace(/\s+/g, '').length === 0;
+    },
+    [],
+  );
+
+  return (
+    <>
+      <span className="mb-2 mt-3 text-12M">
+        공백포함 : 총 {textLength > 0 ? textLength : 0}자 | 공백제외 : 총{' '}
+        {trimmedTextLength > 0 ? trimmedTextLength : 0}자
+      </span>
+      {errors.content && (
+        <ErrorMessage className="ml-0" message={errors.content.message} />
+      )}
+      <div className="relative size-full min-h-60 flex-1 basis-auto overflow-auto">
+        <Controller
+          control={control}
+          name="content"
+          render={({ field: { onChange, value } }) => {
+            return (
+              <QuillNoSSRWrapper
+                forwardedRef={quillInstance}
+                modules={modules}
+                value={value}
+                onChange={(content: string) => {
+                  if (!content || isEmptyContent(content)) {
+                    onChange('');
+                  } else {
+                    onChange(content);
+                  }
+
+                  // 텍스트 길이 계산
+                  if (quillInstance.current) {
+                    const quillEditor = quillInstance.current.getEditor();
+                    const text = quillEditor.getText();
+                    // 전체 문자열에서 모든 공백을 찾아서 제거하는 정규식 활용
+                    const nonSpaceLength = text.replace(/\s/g, '').length;
+                    const length = quillEditor.getLength();
+
+                    setTextLength(length > 1 ? length - 1 : 0);
+                    setTrimmedTextLength(nonSpaceLength);
+                  }
+                }}
+                placeholder="이 곳을 클릭해 노트 작성을 시작해주세요"
+              />
+            );
+          }}
+        />
+      </div>
+    </>
+  );
+}
--- src/components/note/NoteEditor.tsx
+++ src/components/note/NoteEditor.tsx
@@ -1,66 +1,31 @@
 'use client';
 
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import ReactQuill from 'react-quill-new';
+import React from 'react';
 import 'react-quill-new/dist/quill.snow.css';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { faFontAwesome } from '@fortawesome/free-solid-svg-icons/faFontAwesome';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Button from '../common/button/Button';
 import Icon from '../common/icon/Icon';
 import '@/styles/textEditor.css';
 import { NoteSchema } from '@/lib/note-validation';
-import ErrorMessage from '../auth/ErrorMessage';
+import NoteContentEditor from './NoteContentEditor';
+import NoteTitle from './NoteTitle';
 
 export default function NoteEditor() {
-  const [textLength, setTextLength] = useState(0);
-  const quillRef = useRef<ReactQuill>(null);
-
-  const modules = useMemo(() => {
-    // 툴바 옵션들
-    const toolbarOptions = [
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ align: '' }, { align: 'center' }, { align: 'right' }],
-
-      [
-        { list: 'bullet' },
-        { list: 'ordered' },
-        { color: [] },
-        { background: [] },
-      ],
-      ['link'],
-    ];
-
-    return {
-      toolbar: {
-        container: toolbarOptions,
-      },
-    };
-  }, []);
-
   const {
     control,
-    setValue,
     formState: { errors, isValid },
+    getValues,
+    setError,
+    clearErrors,
+    watch,
+    trigger,
   } = useForm({
     resolver: zodResolver(NoteSchema),
     defaultValues: { title: '', content: '' },
     mode: 'onChange',
   });
-
-  useEffect(() => {
-    const quillEditor = quillRef.current?.getEditor(); // Quill 에디터 인스턴스
-
-    if (quillEditor) {
-      // 초기 텍스트 길이 설정
-      setTextLength(quillEditor.getLength() - 1);
-
-      // 텍스트가 변경될 때마다 getLength() 호출
-      quillEditor.on('text-change', () => {
-        setTextLength(quillEditor.getLength() - 1); // 업데이트
-      });
-    }
-  }, []);
 
   return (
     <form className="mx-auto flex h-dvh w-full flex-col overflow-auto break-keep rounded-2xl bg-gs00 p-4 text-gs900 md:px-6 md:py-4">
@@ -85,7 +50,7 @@ export default function NoteEditor() {
             </Button>
           </div>
         </div>
-        {/* 목표 있을 경우 */}
+        {/* goal 목표 있을 경우 */}
         <section className="mb-3 flex items-center gap-2">
           <Icon
             icon={faFontAwesome}
@@ -95,71 +60,25 @@ export default function NoteEditor() {
             자바스크립트로 웹 서비스 만들기
           </h3>
         </section>
-        {/* 할일 상태, 할일 제목 */}
+        {/* todo 상태, todo 제목 */}
         <article className="mb-2 flex items-center gap-2 text-gs700">
           <span className="rounded-md bg-gs100 p-1 text-12M">To do</span>
           <h4 className="text-14R">자바스크립트 기초 챕터1 듣기</h4>
         </article>
       </div>
       <div className="flex h-full min-h-0 flex-col text-gs800">
-        <div className="flex items-center justify-between border-y border-gs200 py-3">
-          <Controller
-            control={control}
-            name="title"
-            render={({ field: { value, onChange } }) => (
-              <>
-                <input
-                  placeholder="노트의 제목을 입력해주세요"
-                  className="border-top border-bottom w-full rounded-none bg-gs00 pl-0 outline-none focus:border-gs200"
-                  onChange={(e) => onChange(e.target.value)}
-                  value={value}
-                />
-                <div className="flex px-1 py-[2px] text-xs font-medium">
-                  <span className="text-error">
-                    {value ? value?.length : 0}
-                  </span>
-                  <span className="text-blue-500">/30</span>
-                </div>
-              </>
-            )}
-          />
-        </div>
-        {errors.title && (
-          <ErrorMessage
-            className="ml-0"
-            message={errors.title?.message || ''}
-          />
-        )}
-        <span className="mb-2 mt-3 text-12M">
-          공백포함 : 총 {textLength > 0 ? textLength : 0}자 | 공백제외 : 총{' '}
-          {textLength > 0 ? textLength : 0}자
-        </span>
-        <div className="relative size-full min-h-60 flex-1 basis-auto overflow-auto">
-          <Controller
-            control={control}
-            name="content"
-            render={({ field: { onChange, value } }) => {
-              return (
-                <ReactQuill
-                  ref={quillRef}
-                  modules={modules}
-                  value={value}
-                  onChange={(content) => {
-                    const trimmedContent = content.trim();
-                    setValue('content', trimmedContent, {
-                      shouldValidate: true,
-                    });
-                    onChange(trimmedContent);
-                  }}
-                  placeholder="이 곳을 클릭해 노트 작성을 시작해주세요"
-                />
-              );
-            }}
-          />
-          {errors.content && (
-            <ErrorMessage message={errors.content?.message || ''} />
-          )}
-        </div>
+        {/* 노트 제목 */}
+        <NoteTitle control={control} errors={errors} />
+        {/* 노트 컨텐츠 영역 */}
+        <NoteContentEditor
+          control={control}
+          errors={errors}
+          watch={watch}
+          getValues={getValues}
+          setError={setError}
+          clearErrors={clearErrors}
+          trigger={trigger}
+        />
       </div>
     </form>
   );
