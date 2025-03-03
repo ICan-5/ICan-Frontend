18386e109b390a6fdb3643eea03314bbfc766c3a
Jiyeon Han <chocorogit@gmail.com>
2025-03-03 13:46:15 +0900

    [CAN-39] feat: 코드잇 노트 생성 API 연동, 노트 생성 후 노트 ID를 이용한 백엔드 할일 수정 API 연동

--- src/app/api/note/[todoId]/route.ts
+++ src/app/api/note/[todoId]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { fetchIntance } from '@/services/fetchInstance';
 
 // 노트 생성
@@ -6,35 +6,48 @@ export async function POST(
   req: NextRequest,
   { params }: { params: { todoId: number } },
 ) {
-  const { todoId } = params;
-  const body = await req.json();
-  const { title, content, linkUrl } = body;
+  try {
+    const { todoId } = params;
+    const body = await req.json();
+    const { title, content, linkUrl } = body;
 
-  console.log(
-    'todoId, title, content, linkUrl',
-    todoId,
-    title,
-    content,
-    linkUrl,
-  );
+    const res1 = await fetchIntance({
+      base: 'CODEIT',
+      method: 'POST',
+      url: '/notes',
+      body: {
+        todoId: Number(todoId), // 숫자 변환
+        title,
+        content,
+        linkUrl,
+      },
+    });
 
-  const res1 = await fetchIntance({
-    base: 'CODEIT',
-    method: 'POST',
-    url: '/notes',
-    body: { todoId, title, content, linkUrl },
-  });
+    if (!res1.ok) return res1;
 
-  if (!res1.ok) return res1;
+    const data = await res1.json();
 
-  const data = await res1.json();
-  const { noteId } = data;
+    // 노트 id
+    const { id: noteId } = data;
 
-  const res2 = await fetchIntance({
-    base: 'BACKEND',
-    method: 'PATCH',
-    url: `/todos/${todoId}`,
-    body: { noteId },
-  });
-  return res2;
+    // 노트 id를 해당 todo에 업데이트
+    const res2 = await fetchIntance({
+      base: 'BACKEND',
+      method: 'PATCH',
+      url: `/todos/${todoId}`,
+      body: { noteId },
+    });
+
+    const data2 = await res2.json();
+
+    return NextResponse.json(
+      { message: '노트 생성이 완료되었습니다.', data: data2 },
+      { status: 200 },
+    );
+  } catch {
+    return NextResponse.json(
+      { message: '노트 생성 중 에러가 발생했습니다.' },
+      { status: 500 },
+    );
+  }
 }
