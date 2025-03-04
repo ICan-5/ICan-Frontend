70a4b3d589abc5720a3db071232bcc697631fa0b
Jiyeon Han <chocorogit@gmail.com>
2025-03-04 11:01:51 +0900

    [CAN-39] fix: dev 충돌 수정

--- src/components/note/NoteEditor.tsx
+++ src/components/note/NoteEditor.tsx
@@ -5,16 +5,23 @@ import 'react-quill-new/dist/quill.snow.css';
 import { useForm } from 'react-hook-form';
 import { faFontAwesome } from '@fortawesome/free-solid-svg-icons/faFontAwesome';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useParams, useRouter } from 'next/navigation';
 import Button from '../common/button/Button';
 import Icon from '../common/icon/Icon';
 import '@/styles/textEditor.css';
-import { NoteSchema } from '@/lib/note-validation';
+import { NoteSchema, NoteSchemaType } from '@/lib/note-validation';
 import NoteContentEditor from './NoteContentEditor';
 import NoteTitle from './NoteTitle';
+import { createNote } from '@/services/note';
+import { useTodoWithGoalTitle } from '@/hooks/useTodoWithGoalTitle';
+import NoteTitlesSkeleton from './NoteTitlesSkeleton';
 
 export default function NoteEditor() {
+  const { todoId } = useParams<{ todoId: string }>();
+  const router = useRouter();
   const {
     control,
+    handleSubmit,
     formState: { errors, isValid },
     getValues,
     setError,
@@ -27,8 +34,36 @@ export default function NoteEditor() {
     mode: 'onChange',
   });
 
+  // 할 일 제목, 목표 제목 가져오기
+  const { todoQuery, goalQuery } = useTodoWithGoalTitle(todoId);
+
+  const onSubmit = async (formData: NoteSchemaType) => {
+    if (todoId) {
+      try {
+        const res = await createNote({
+          todoId: Number(todoId),
+          formData,
+        });
+
+        // console.log('res________', res);
+
+        if (res) {
+          alert('노트 생성이 완료됐습니다.');
+          router.back();
+        }
+      } catch (error) {
+        console.error('노트 생성 중 오류 발생1:', error);
+      }
+    } else {
+      console.error('todoId가 없습니다.');
+    }
+  };
+
   return (
-    <form className="mx-auto flex h-dvh w-full flex-col overflow-auto break-keep rounded-2xl bg-gs00 p-4 text-gs900 md:px-6 md:py-4">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mx-auto flex h-dvh w-full flex-col overflow-auto break-keep rounded-2xl bg-gs00 p-4 text-gs900 md:px-6 md:py-4"
+    >
       <div>
         <div className="mb-4 w-full items-center justify-between xs:flex">
           <h2 className="text-16SB xs:text-18SB">노트 작성</h2>
@@ -51,20 +86,28 @@ export default function NoteEditor() {
           </div>
         </div>
         {/* goal 목표 있을 경우 */}
-        <section className="mb-3 flex items-center gap-2">
-          <Icon
-            icon={faFontAwesome}
-            className="rounded-lg bg-gs800 text-xs text-gs00"
-          />
-          <h3 className="text-16M text-gs800">
-            자바스크립트로 웹 서비스 만들기
-          </h3>
-        </section>
-        {/* todo 상태, todo 제목 */}
-        <article className="mb-2 flex items-center gap-2 text-gs700">
-          <span className="rounded-md bg-gs100 p-1 text-12M">To do</span>
-          <h4 className="text-14R">자바스크립트 기초 챕터1 듣기</h4>
-        </article>
+        {todoQuery.isLoading || goalQuery.isLoading ? (
+          <NoteTitlesSkeleton />
+        ) : (
+          <>
+            {goalQuery.data?.todo.title && (
+              <section className="mb-3 flex items-center gap-2">
+                <Icon
+                  icon={faFontAwesome}
+                  className="rounded-lg bg-gs800 text-xs text-gs00"
+                />
+                <h3 className="text-16M text-gs800">
+                  {goalQuery.data?.todo.title}
+                </h3>
+              </section>
+            )}
+
+            <article className="mb-2 flex items-center gap-2 text-gs700">
+              <span className="rounded-md bg-gs100 p-1 text-12M">To do</span>
+              <h4 className="text-14R">{todoQuery.data?.title}</h4>
+            </article>
+          </>
+        )}
       </div>
       <div className="flex h-full min-h-0 flex-col text-gs800">
         {/* 노트 제목 */}
