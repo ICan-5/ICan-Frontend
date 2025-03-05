06d59caed507e89f45966a6dd04ed8e1bd489b8f
minji kim <kmj_1027@naver.com>
2025-03-05 16:51:42 +0900

    [CAN-43] feat: FormField컴포넌트 개발, schema 생성

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -71,6 +71,26 @@ export const LoginSchema = z.object({
     .max(12, { message: ERROR_MESSAGE.password.max }),
 });
 
+export const SettingSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: '닉네임은 최소 1자 이상이어야 합니다.' })
+    .max(10, '닉네임은 10자 이하여야 합니다')
+    .optional(),
+  profile: z.instanceof(File).optional(),
+  currentPassword: z.string().optional(),
+  newPassword: z.preprocess(
+    (value) => (value === '' ? undefined : value),
+    z
+      .string()
+      .min(8, { message: ERROR_MESSAGE.password.min })
+      .max(12, { message: ERROR_MESSAGE.password.max })
+      .optional(),
+  ),
+  confirmPassword: z.string().optional(),
+});
+
 // 스키마의 z.infer를 사용하여 스키마 유형도 내보내기
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
 export type SignUpSchemaType = z.infer<typeof SignUpSchema>;
+export type SettingSchemaType = z.infer<typeof SettingSchema>;
