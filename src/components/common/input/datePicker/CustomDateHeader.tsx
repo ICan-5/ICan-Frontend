28a4d2284ee6a5fd01c916b75f2ab819246173bd
izodam <yeon91601@gmail.com>
2025-03-07 13:37:47 +0900

    [CAN-60] style: date picker 디자인 적용

---
+++ src/components/common/input/datePicker/CustomDateHeader.tsx
@@ -0,0 +1,33 @@
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { getMonth, getYear } from 'date-fns';
+import IconButton from '../../button/IconButton';
+
+interface Props {
+  date: Date;
+  decreaseMonth: () => void;
+  increaseMonth: () => void;
+}
+
+export default function CustomDateHeader({
+  date,
+  decreaseMonth,
+  increaseMonth,
+}: Props) {
+  return (
+    <div className="flex justify-between">
+      <IconButton
+        icon={faAngleLeft}
+        onClick={decreaseMonth}
+        className="text-gsBk"
+      />
+      <span className="text-18M text-gsBk">
+        {getYear(date)}년 {getMonth(date) + 1}월
+      </span>
+      <IconButton
+        icon={faAngleRight}
+        onClick={increaseMonth}
+        className="text-gsBk"
+      />
+    </div>
+  );
+}
