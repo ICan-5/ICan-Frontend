30dcb4b527cdfda56ba90c30650ba7ad1824e9af
jeongeunhahq <dmsgk991@naver,com>
2025-03-08 19:50:27 +0900

    [CAN-65] chore: 할일 장바구니 style 변경'

--- src/components/goalDetail/GoalBasket.tsx
+++ src/components/goalDetail/GoalBasket.tsx
@@ -26,6 +26,7 @@ export default function GoalBasket({ basketItems, goalId }: Props) {
   );
   const addTodoMutation = useGoalAddTodo();
   const deleteTodoMutation = useDeleteBasketTodo();
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
 
   const handleDateSelect = (date: Date | null, item: Basket) => {
     if (!date) return;
@@ -54,85 +55,97 @@ export default function GoalBasket({ basketItems, goalId }: Props) {
       setSelectedDeleteTodo(null);
     }
   };
-
   return (
     <div className="relative flex h-[285px] flex-col rounded-2xl shadow">
       <div className="relative mb-4 flex items-center rounded-t-2xl border-b bg-gs00 p-4">
-        <div className="group relative flex items-center gap-2">
+        <div className="relative flex items-center gap-2">
           <h3 className="text-18SB">할일 장바구니</h3>
-          <FontAwesomeIcon
-            icon={faCircleQuestion}
-            className="cursor-pointer text-gs500"
-          />
-
-          {/* 툴팁 */}
-          <div className="absolute top-full mt-2 w-max rounded-md bg-gs00 px-3 py-2 text-14M text-gsBk opacity-0 shadow transition-opacity group-hover:opacity-100">
-            할 일을 미리 입력해 두고
-            <br />
-            원하는 날짜에 지정할 수 있습니다.
-            <br />
-            급하게 떠오른 할 일도 쉽게 저장하고 관리하세요!
+          <div
+            className="relative flex items-center"
+            onMouseEnter={() => setIsTooltipOpen(true)}
+            onMouseLeave={() => setIsTooltipOpen(false)}
+          >
+            <FontAwesomeIcon
+              icon={faCircleQuestion}
+              className="cursor-pointer text-gs500"
+            />
+            <div
+              className={`absolute top-full -ml-12 mt-2 min-w-[205px] rounded-md bg-gs00 p-2 text-14M text-gsBk shadow transition-opacity duration-200 ${
+                isTooltipOpen ? 'opacity-100' : 'pointer-events-none opacity-0'
+              }`}
+            >
+              할 일을 미리 입력해 두고
+              <br />
+              원하는 날짜에 지정할 수 있습니다.
+              <br />
+              급하게 떠오른 할 일도 쉽게 저장하고 관리하세요!
+            </div>
           </div>
         </div>
-
         <p className="ml-auto cursor-pointer text-gs500">모두 지우기</p>
       </div>
-
-      <ul className="list-none space-y-2 overflow-y-auto px-6">
-        {basketItems &&
-          basketItems.map((item) => (
-            <li
-              key={item.id}
-              className={`flex items-center justify-between border-b border-dashed border-gs300 pb-2 text-gs700 transition-colors ${
-                hoveredItem === item.id ? 'text-slate500' : 'text-gs700'
-              }`}
-              onMouseEnter={() => setHoveredItem(item.id)}
-              onMouseLeave={() => setHoveredItem(null)}
-            >
-              <span>{item.title}</span>
-              <div className="flex items-center space-x-3">
-                <div className="relative flex items-center">
-                  <DatePicker
-                    dateFormat="yyyy-MM-dd"
-                    selected={null}
-                    portalId="root-portal"
-                    onChange={(date: Date | null) =>
-                      handleDateSelect(date, item)
-                    }
-                    customInput={
-                      <button
-                        type="button"
-                        className={`flex size-7 items-center justify-center rounded-full bg-white p-1 shadow-md transition-opacity duration-200 ${
-                          hoveredItem === item.id ? 'opacity-100' : 'opacity-0'
-                        }`}
-                      >
-                        <FontAwesomeIcon
-                          icon={faCalendar}
-                          className="text-slate400"
-                        />
-                      </button>
-                    }
-                  />
+      <div className="flex-1 overflow-y-auto px-6">
+        <ul className="list-none space-y-2">
+          {basketItems &&
+            basketItems.map((item) => (
+              <li
+                key={item.id}
+                className={`flex items-center justify-between border-b border-dashed border-gs300 pb-2 text-gs700 transition-colors ${
+                  hoveredItem === item.id ? 'text-slate500' : 'text-gs700'
+                }`}
+                onMouseEnter={() => setHoveredItem(item.id)}
+                onMouseLeave={() => setHoveredItem(null)}
+              >
+                <span>{item.title}</span>
+                <div className="flex items-center space-x-3">
+                  <div className="relative flex items-center">
+                    <DatePicker
+                      dateFormat="yyyy-MM-dd"
+                      selected={null}
+                      portalId="root-portal"
+                      onChange={(date: Date | null) =>
+                        handleDateSelect(date, item)
+                      }
+                      customInput={
+                        <button
+                          type="button"
+                          className={`flex size-7 items-center justify-center rounded-full bg-gs00 p-1 shadow-md transition-opacity duration-200 ${
+                            hoveredItem === item.id
+                              ? 'opacity-100'
+                              : 'opacity-0'
+                          }`}
+                        >
+                          <FontAwesomeIcon
+                            icon={faCalendar}
+                            className="text-slate400"
+                          />
+                        </button>
+                      }
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className={`flex size-7 items-center justify-center rounded-full bg-gs00 p-1 shadow-md transition-opacity duration-200 ${
+                      hoveredItem === item.id ? 'opacity-100' : 'opacity-0'
+                    }`}
+                    onClick={() => setSelectedDeleteTodo(item.id)}
+                  >
+                    <FontAwesomeIcon
+                      icon={faTrashCan}
+                      className="text-warn500"
+                    />
+                  </button>
                 </div>
-                <button
-                  type="button"
-                  className={`flex size-7 items-center justify-center rounded-full bg-white p-1 shadow-md transition-opacity duration-200 ${
-                    hoveredItem === item.id ? 'opacity-100' : 'opacity-0'
-                  }`}
-                  onClick={() => setSelectedDeleteTodo(item.id)}
-                >
-                  <FontAwesomeIcon icon={faTrashCan} className="text-warn500" />
-                </button>
-              </div>
-            </li>
-          ))}
-      </ul>
-      {(!basketItems || basketItems.length === 0) && (
-        <p className="flex h-full items-center justify-center py-6 text-gs500">
-          장바구니에 할일이 없습니다.
-        </p>
-      )}
-      <div className="sticky z-10 rounded-b-xl bg-gs100 p-5">
+              </li>
+            ))}
+        </ul>
+        {(!basketItems || basketItems.length === 0) && (
+          <p className="flex h-full items-center justify-center py-6 text-gs500">
+            장바구니에 할일이 없습니다.
+          </p>
+        )}
+      </div>
+      <div className="absolute bottom-0 left-0 w-full rounded-b-xl bg-gs100 p-5">
         <div
           className="flex cursor-pointer items-center justify-center rounded-xl border-2 border-slate500 p-1 text-14M text-slate500"
           onClick={handleAddTodo}
@@ -140,6 +153,7 @@ export default function GoalBasket({ basketItems, goalId }: Props) {
           + 장바구니에 새 할일 추가
         </div>
       </div>
+
       {isModalOpen && (
         <BasketTodoModal
           onClose={() => setIsModalOpen(false)}
