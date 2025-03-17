8806c9a1f4122c5e37364f1bf5e5b9140dc9e65e
minji kim <kmj_1027@naver.com>
2025-03-17 10:44:51 +0900

    [CAN-74] fix: queryClinet staleTime default설정, addTodo, updateTodo invalidate goal_todo 추가

--- src/components/common/ClientProvider.tsx
+++ src/components/common/ClientProvider.tsx
@@ -5,7 +5,16 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
 export default function ClientProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 60 * 1000,
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
